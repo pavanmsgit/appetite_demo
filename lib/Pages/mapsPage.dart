f279508d4e64a967777a97caeca04af5387a8066
Siddhika <siddhikam18@gmail.com>
2021-04-21 18:25:59 +0530

    Basic home page and navigation pages added

---
+++ lib/Pages/mapsPage.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+// import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:firebase_database/firebase_database.dart';
+
+class MapsPage extends StatefulWidget {
+  @override
+  _MapsPageState createState() => _MapsPageState();
+}
+
+class _MapsPageState extends State<MapsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('Map'),
+    );
+  }
+}
